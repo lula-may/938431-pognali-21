05a56a7767c6a2b729f99ada9bf75cecf9a7fe6b
Yulia Elagina <julia-elagina@yandex.ru>
2021-01-13 01:51:25 +0300

    Добавляет скрипты и стили для кастомной валидации полей форм

--- source/js/activity.js
+++ source/js/activity.js
@@ -1,17 +1,4 @@
-const directionFormElement = document.querySelector('#direction-form');
-const textareaElements = directionFormElement.querySelectorAll('.activity__text');
+const form = document.querySelector('#direction-form');
+const textareaElements = form.querySelectorAll('.activity__text');
 
-directionFormElement.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  console.log('validity:', directionFormElement.reportValidity());
-  return (directionFormElement.reportValidity())
-    ? directionFormElement.submit()
-    : directionFormElement.classList.add('direction-form--invalid');
-})
-
-textareaElements.forEach((element) => {
-  element.addEventListener('change', () => {
-    console.log('change', element.id);
-    directionFormElement.classList.remove('direction-form--invalid');
-  })
-})
+window.form.setSubmitHandler(form, textareaElements, 'direction-form');
---
+++ source/js/form.js
@@ -0,0 +1,24 @@
+(function () {
+  const INVALID_POSTFIX = '--invalid';
+
+  const setSubmitHandler = (form, elements, formClass) => {
+    const invalidClass = `${formClass}${INVALID_POSTFIX}`;
+
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      return (form.reportValidity())
+        ? form.submit()
+        : form.classList.add(invalidClass);
+    });
+
+    elements.forEach((element) => {
+      element.addEventListener('focus', () => {
+        form.classList.remove(invalidClass);
+      });
+    });
+  };
+
+  window.form = {
+    setSubmitHandler
+  };
+})();
