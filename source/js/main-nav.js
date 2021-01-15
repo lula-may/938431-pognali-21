df0f578e4a79b2a8c5d7e61419a134cfc2155557
Yulia Elagina <julia-elagina@yandex.ru>
2021-01-16 01:50:20 +0300

    Подключает скрипты для плавного скролла якорных ссылок и для меню

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,12 +1,19 @@
-document.querySelector(`body`).classList.remove(`page__body--no-js`);
+const bodyElement = document.querySelector(`body`);
+bodyElement.classList.remove(`page__body--no-js`);
 
 const mainNav = document.querySelector(`.main-nav`);
 const mainNavToggle = mainNav.querySelector(`.main-nav__toggle`);
 
+const onScroll = () => {
+  (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+  ? bodyElement.classList.add(`page__body--scroll`)
+  : bodyElement.classList.remove(`page__body--scroll`);
+};
+
 mainNavToggle.addEventListener(`click`, () => {
   mainNav.classList.toggle(`main-nav--open`);
   mainNav.classList.toggle(`main-nav--close`);
-})
+});
 
 mainNavToggle.addEventListener(`keydown`, (evt) => {
   if (evt.key !== `Enter`) {
@@ -15,4 +22,6 @@ mainNavToggle.addEventListener(`keydown`, (evt) => {
   evt.preventDefault();
   mainNav.classList.toggle(`main-nav--open`);
   mainNav.classList.toggle(`main-nav--close`);
-})
+});
+
+document.addEventListener('scroll', onScroll);
